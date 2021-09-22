f3d05cb02e7417228de4dc7572661cc5892b0a43
Alexey Kovaliov <akovalev@aristeksystems.com>
2021-09-22 20:11:09 +0300

    add row update

--- src/components/table/components/row/components/add-row/duck/utils.ts
+++ src/components/table/components/row/components/add-row/duck/utils.ts
@@ -3,78 +3,102 @@ import { emptyCol } from "../../../../../duck/constants";
 
 export const generateNewRows = (rows: RowType[], rowId: number): RowType[] => {
   const prevRow = rows[rowId - 1];
-  const increasedCols: string[] = [];
+  const nextRow = rows[rowId];
   const newRow: RowType = {
     id: rowId + 1,
     cols: [],
   };
 
   prevRow.cols.forEach((col: ColType, index) => {
-    if (col.rowSpan) {
-      const colID = `${prevRow.id}${col.id}`;
-      if (!increasedCols.includes(colID)) {
-        rows[rowId - 1].cols[index].rowSpan = col.rowSpan + 1;
-        increasedCols.push(colID);
-      }
+    const newCol = { ...emptyCol, id: col.id };
 
-      newRow.cols.push({
-        ...emptyCol,
-        id: col.id,
-        display: false,
-        resourceFor: { rowId: rowId, colId: col.id },
-        ...(col.colSpan ? { colSpan: col.colSpan } : {}),
-      });
-    } else if (col.resourceFor) {
-      newRow.cols.push({
-        ...emptyCol,
-        id: col.id,
-        resourceFor: col.resourceFor,
-        display: false,
-        ...(col.colSpan ? { colSpan: col.colSpan } : {}),
-      });
+    if (nextRow) {
+      const prevCol = prevRow.cols[index];
+      const nextCol = nextRow.cols[index];
 
-      if (col.resourceFor.rowId !== prevRow.id) {
-        const colID = `${col.resourceFor.rowId}${col.resourceFor.colId}`;
-        if (!increasedCols.includes(colID)) {
-          // @ts-ignore
-          rows[col.resourceFor.rowId - 1].cols[
-            col.resourceFor.colId - 1
-          ].rowSpan += 1;
-          increasedCols.push(colID);
-        }
+      if (
+        prevCol.rowSpan &&
+        nextCol.resourceFor &&
+        nextCol.resourceFor.colId === prevCol.id &&
+        nextCol.resourceFor.rowId === prevRow.id
+      ) {
+        rows[rowId - 1].cols[index].resources?.push({
+          rowId: rowId + 2,
+          colId: col.id,
+        });
+
+        newRow.cols.push({
+          ...newCol,
+          display: false,
+          resourceFor: { rowId, colId: col.id },
+        });
+      } else if (
+        prevCol.resourceFor &&
+        nextCol.resourceFor &&
+        nextCol.resourceFor.colId === prevCol.resourceFor.colId &&
+        nextCol.resourceFor.rowId === prevCol.resourceFor.rowId
+      ) {
+        rows[prevCol.resourceFor.rowId - 1].cols[
+          prevCol.resourceFor.colId - 1
+        ].resources?.push({
+          rowId: rowId + 2,
+          colId: col.id,
+        });
+
+        newRow.cols.push({
+          ...newCol,
+          display: false,
+          resourceFor: prevCol.resourceFor,
+        });
+      } else {
+        newRow.cols.push(newCol);
       }
     } else {
-      newRow.cols.push({
-        ...emptyCol,
-        id: col.id,
-        ...(col.colSpan ? { colSpan: col.colSpan } : {}),
-      });
+      newRow.cols.push(newCol);
+    }
+  });
+
+  const increasedCols: string[] = [];
+
+  newRow.cols.forEach((col) => {
+    if (col.resourceFor) {
+      const colId = `${col.resourceFor.rowId}${col.resourceFor.colId}`;
+
+      if (!increasedCols.includes(colId)) {
+        const rowSpan =
+          rows[col.resourceFor.rowId - 1].cols[col.resourceFor.colId - 1]
+            .rowSpan || 0;
+
+        rows[col.resourceFor.rowId - 1].cols[
+          col.resourceFor.colId - 1
+        ].rowSpan = rowSpan + 1;
+
+        increasedCols.push(colId);
+      }
     }
   });
 
   const newRows = rows.map((row: RowType) => {
     if (row.id > rowId) {
-      let newCols = row.cols.map((col: ColType) => {
-        return {
-          ...col,
-          ...(col.resources
-            ? {
-                resources: col.resources.map((currentResource) => ({
-                  ...currentResource,
-                  rowId: currentResource.rowId + 1,
-                })),
-              }
-            : {}),
-          ...(col.resourceFor && col.resourceFor.rowId > rowId
-            ? {
-                resourceFor: {
-                  ...col.resourceFor,
-                  rowId: col.resourceFor.rowId + 1,
-                },
-              }
-            : col.resourceFor),
-        };
-      });
+      let newCols = row.cols.map((col: ColType) => ({
+        ...col,
+        ...(col.resources
+          ? {
+              resources: col.resources.map((currentResource) => ({
+                ...currentResource,
+                rowId: currentResource.rowId + 1,
+              })),
+            }
+          : {}),
+        ...(col.resourceFor && col.resourceFor.rowId > rowId
+          ? {
+              resourceFor: {
+                ...col.resourceFor,
+                rowId: col.resourceFor.rowId + 1,
+              },
+            }
+          : col.resourceFor),
+      }));
 
       return { ...row, id: row.id + 1, cols: newCols };
     }
